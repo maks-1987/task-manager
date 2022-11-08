ad9fa539455dfa1d1961b51c1374d0855a726726
MaxD <max.moderation@gmail.com>
2022-11-08 22:11:55 +0200

    feat: add default endpoints file

---
+++ src/endpoints/endpoints.ts
@@ -0,0 +1,11 @@
+const URL = 'https://http://localhost:4000';
+
+export const Endpoints = {
+  ROOT: `${URL}`,
+  USERS: `${URL}/users`,
+  SIGN_IN: `${URL}/signin`,
+  SIGN_UP: `${URL}/signup`,
+  BOARDS: `${URL}/boards`,
+  FILE: `${URL}/file`,
+  LOGS: `${URL}/logs`,
+};
