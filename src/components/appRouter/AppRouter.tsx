774a16b57485270a929809b2949676142e60c46a
matvey84 <matvey8407@mail.ru>
2022-11-08 17:06:05 +0300

    refactor: rename app folder to redux folder,  moved pages folder from components folder

--- src/components/appRouter/AppRouter.tsx
+++ src/components/appRouter/AppRouter.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import BoardsPage from '../pages/boards-page/BoardsPage';
-import NotFoundPage from '../pages/not-found-page/NotFoundPage';
-import WelcomePage from '../pages/welcome-page/WelcomePage';
+import BoardsPage from '../../pages/boards-page/BoardsPage';
+import NotFoundPage from '../../pages/not-found-page/NotFoundPage';
+import WelcomePage from '../../pages/welcome-page/WelcomePage';
 import Layout from './Layout';
 
 export default function AppRouter() {
