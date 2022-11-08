6e4c8ca7c515a122ebe28eead2a10e985d868477
matvey84 <matvey8407@mail.ru>
2022-11-08 14:06:27 +0300

    feat:  import BrowserRouter

--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,17 @@ import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
