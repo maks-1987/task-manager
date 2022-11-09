5867111fd2135dc398392155f58b97b9709d1306
MaxD <max.moderation@gmail.com>
2022-11-09 08:40:36 +0200

    feat: add combine reducer

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,12 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import { configureStore, ThunkAction, Action, combineReducers } from '@reduxjs/toolkit';
 
+const rootReducer = combineReducers({});
 export const store = configureStore({
-  reducer: {},
+  reducer: rootReducer,
 });
 
 export type AppDispatch = typeof store.dispatch;
-export type RootState = ReturnType<typeof store.getState>;
+export type RootState = ReturnType<typeof rootReducer>;
 export type AppThunk<ReturnType = void> = ThunkAction<
   ReturnType,
   RootState,
