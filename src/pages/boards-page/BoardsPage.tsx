dadbae6a0c222980f7788935cfe74c4ea7d74a90
matvey84 <matvey8407@mail.ru>
2022-11-10 13:15:43 +0300

    feat: add CreateBoardsForm

--- src/pages/boards-page/BoardsPage.tsx
+++ src/pages/boards-page/BoardsPage.tsx
@@ -1,5 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import CreateBoardForm from '../../components/createBoardForm/CreateBoardForm';
+import { Endpoints } from '../../endpoints/endpoints';
+import { IPreBoard } from '../../types/types';
+import './borderPage.css';
+
+export const tokeMok =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI0NGNmOTZkMi01OGZjLTRlMGMtOTZkOS05YWM0MjhkNGQ0OTUiLCJsb2dpbiI6InVzZXIwMDEiLCJpYXQiOjE2NTIwMDMyMTF9.EUlvrrs0Hl7wq1o-vkW5eh710CeNmhTfivk8aYkO43I';
 
 export default function BoardsPage() {
-  return <div>BoardsPage</div>;
+  const [board, setBoard] = useState<IPreBoard>({
+    id: '',
+    title: '',
+  });
+  const [boards, setBoards] = useState<IPreBoard[]>([]);
+
+  const getBoards = async (token: string) => {
+    const response = await fetch(Endpoints.BOARDS, {
+      method: 'GET',
+      headers: {
+        'Content-type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const data = await response.json();
+    setBoards(data);
+  };
+  useEffect(() => {
+    return setBoards((boards) => [...boards, board]);
+  }, [board]);
+
+  useEffect(() => {
+    getBoards(tokeMok);
+  }, []);
+  console.log(boards);
+  return (
+    <section className="boards-page">
+      <div className="boards-page__container">
+        <h1 className="boards-page__title">Boards Page</h1>
+        <CreateBoardForm setBoard={setBoard} />
+        <section className="boards-page__container_boards-field">
+          {boards.length === 0
+            ? 'You have not any boards'
+            : boards.map((board) => <span key={board.id}>board.title</span>)}
+        </section>
+      </div>
+    </section>
+  );
 }
