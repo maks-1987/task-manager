3481504e0e7ff0ceacd223130c6b1b08be37ee6e
DenisWilk <info.d1@tut.by>
2022-11-11 20:10:36 +0300

    feat: add header layout and initial syles

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,5 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css';
 
-export default function Header() {
-  return <div>Header</div>;
+function Header(): JSX.Element {
+  return (
+    <header className="header">
+      <nav className="nav">
+        <Link className="nav__link" to="boards/:id">
+          Create board
+        </Link>
+
+        <Link className="nav__link" to="profile">
+          Edit profile
+        </Link>
+
+        <Link className="nav__link" to="logout">
+          Sign Out
+        </Link>
+
+        <select className="nav__select">
+          <option value="EN">EN</option>
+          <option value="UA">UA</option>
+          <option value="BY">BY</option>
+          <option value="RU">RU</option>
+        </select>
+      </nav>
+    </header>
+  );
 }
+
+export default Header;
