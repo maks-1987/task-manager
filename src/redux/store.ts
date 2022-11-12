19385217eb567fc4a402b83562a1827e7490c078
MaxD <max.moderation@gmail.com>
2022-11-12 14:43:10 +0200

    feat: add global modal window

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -11,6 +11,7 @@ import {
 } from 'redux-persist';
 import userSlice from './user-slice/userSlice';
 import storage from 'redux-persist/lib/storage';
+import modalSlice from './modal-slice/modalSlice';
 
 const persistConfig = {
   key: 'root',
@@ -20,6 +21,7 @@ const persistConfig = {
 
 const rootReducer = combineReducers({
   userSlice,
+  modalSlice,
 });
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
