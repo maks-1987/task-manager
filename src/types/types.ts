a8dbd6e4d82f12873dfcbaba2cb6bf258e4fe5bf
MaxD <max.moderation@gmail.com>
2022-11-13 15:53:00 +0200

    feat: add board types

--- src/types/types.ts
+++ src/types/types.ts
@@ -9,3 +9,41 @@ export interface IUserForm {
   login: string;
   password?: string;
 }
+
+export interface IBoard {
+  id: string;
+  title: string;
+  description: string;
+  columns?: IColumn;
+}
+
+export interface IColumn {
+  id: string;
+  title: string;
+  order: number;
+  tasks: ITask;
+}
+
+export interface ITask {
+  id: string;
+  title: string;
+  order: number;
+  description: string;
+  userId: string;
+  boardId: string;
+  columnId: string;
+  files: IFiles;
+}
+
+export interface IFiles {
+  filename: string;
+  fileSize: number;
+}
+
+export type UserId = {
+  [key: number]: string;
+};
+
+export type BoardId = {
+  [key: number]: string;
+};
