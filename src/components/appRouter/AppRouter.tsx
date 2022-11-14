d3d50a582b054d0c0a65fe965a7e0bbee922cdce
MaxD <max.moderation@gmail.com>
2022-11-14 14:37:04 +0200

    feat: add redirect & styles

--- src/components/appRouter/AppRouter.tsx
+++ src/components/appRouter/AppRouter.tsx
@@ -6,7 +6,6 @@ import WelcomePage from '../../pages/welcome-page/WelcomePage';
 import Layout from './Layout';
 import { RegisterPage } from '../../pages/register-page/RegisterPage';
 import { LoginPage } from '../../pages/login-page/LoginPage';
-import { ProjectBoard } from '../../pages/project-board/ProjectBoard';
 import SingleBoard from '../../pages/single-board-page/SingleBoard';
 
 export default function AppRouter() {
@@ -14,8 +13,8 @@ export default function AppRouter() {
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<WelcomePage />} />
+        <Route path="boards/:user" element={<BoardsPage />} />
         <Route path="boards/:user/:boardId" element={<SingleBoard />} />
-        <Route path="board" element={<ProjectBoard />} />
         <Route path="login" element={<LoginPage />} />
         <Route path="register" element={<RegisterPage />} />
         <Route path="*" element={<NotFoundPage />} />
