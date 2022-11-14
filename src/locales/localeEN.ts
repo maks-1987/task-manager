f7ca83665528d2f896fd1513e497d9821738220e
matvey84 <matvey8407@mail.ru>
2022-11-14 10:51:27 +0300

    feat: add new constatn in to lacaleEn object

--- src/locales/localeEN.ts
+++ src/locales/localeEN.ts
@@ -9,5 +9,13 @@ export const localeEN = {
   FORM_MESSAGE_MIN_LENGTH: 'min length 3 symbols',
   FORM_MESSAGE_MAX_LENGTH: 'max length 20 symbols',
   FORM_MESSAGE_LETTERS: 'only letters',
-  ADD_BOARD_TOOLTIP_MESSAGE: 'Both input fields must be filled',
+  boardsContet: {
+    ADD_BOARD_TOOLTIP_MESSAGE: 'Both input fields must be filled',
+    ADD_NEW_BOARD_BUTTON_LABEL: 'Add new board',
+    HAVE_NOT_BOARD_MESSAGE: 'You have not any boards',
+  },
+  modalContetntMessage: {
+    REMOVE_BOARD_CONFIRM_MESSAGE: `If you want to delete the board, confirm the action.`,
+    CREATE_NEW_BOARD_MESSAGE: 'Create a new task board.',
+  },
 };
