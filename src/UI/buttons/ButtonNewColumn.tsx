535828805faf7c26d33fc0c2bcb20c1f2c6c5193
matvey84 <matvey8407@mail.ru>
2022-11-15 20:57:08 +0300

    feat: add ButtonNewColumn component with handler

--- src/UI/buttons/ButtonNewColumn.tsx
+++ src/UI/buttons/ButtonNewColumn.tsx
@@ -1,6 +1,17 @@
 import React from 'react';
+import { useAppDispatch } from '../../redux/hooks';
+import { setIsCreateColumnOrTask, setModalOpen } from '../../redux/modal-slice/modalSlice';
 import './buttons.css';
 
 export const ButtonNewColumn = () => {
-  return <button className="column-item__add-column">Add new column</button>;
+  const dispatch = useAppDispatch();
+  const addColumnButtonHandler = () => {
+    dispatch(setModalOpen(true));
+    dispatch(setIsCreateColumnOrTask(true));
+  };
+  return (
+    <button className="column-item__add-column" onClick={addColumnButtonHandler}>
+      Add new column
+    </button>
+  );
 };
