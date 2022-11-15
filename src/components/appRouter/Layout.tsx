6c63e459dda1767dc02c5545bbc75526378aa643
matvey84 <matvey8407@mail.ru>
2022-11-15 20:28:10 +0300

    refactor: moved GlobalModal to Laypot componnet

--- src/components/appRouter/Layout.tsx
+++ src/components/appRouter/Layout.tsx
@@ -1,13 +1,29 @@
 import React from 'react';
-import { Outlet, useLocation } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
+import { localeEN } from '../../locales/localeEN';
+import { useAppSelector } from '../../redux/hooks';
+import { GlobalModal } from '../../UI/global-modal/GlobalModal';
 import Footer from '../footer/Footer';
+import ColumnsAndTaskForm from '../forms/columnsAndTaskForm/ColumnsAndTaskForm';
+import CreateBoardForm from '../forms/createBoardForm/CreateBoardForm';
 import Header from '../header/Header';
 
 export default function Layout() {
+  const isModalOpen = useAppSelector((state) => state.modalSlice.isModalOpen);
+  const isRemoveBoard = useAppSelector((state) => state.modalSlice.isRemoveBoard);
+  const isCreateColumnsOrTask = useAppSelector((state) => state.modalSlice.isCreateColumnOrTask);
+  const currentPropComponent = isRemoveBoard ? (
+    localeEN.modalContetntMessage.REMOVE_BOARD_CONFIRM_MESSAGE
+  ) : isCreateColumnsOrTask ? (
+    <ColumnsAndTaskForm />
+  ) : (
+    <CreateBoardForm />
+  );
   return (
     <>
       <Header />
       <div className="container">
+        {isModalOpen && <GlobalModal component={currentPropComponent} />}
         <Outlet />
       </div>
       <Footer />
--- src/pages/boards-page/BoardsPage.tsx
+++ src/pages/boards-page/BoardsPage.tsx
@@ -1,12 +1,10 @@
 import React, { useEffect } from 'react';
 import BoardPreviewItem from '../../components/boards/board-preview-item/BoardPreviewItem';
-import CreateBoardForm from '../../components/boards/createBoardForm/CreateBoardForm';
 import Loader from '../../components/loader/Loader';
 import { localeEN } from '../../locales/localeEN';
 import { fetchGetUserBoards } from '../../redux/boards-slice/boardsSlice';
 import { useAppDispatch, useAppSelector } from '../../redux/hooks';
 import AddBoardButton from '../../UI/add-board-button/AddBoardButton';
-import { GlobalModal } from '../../UI/global-modal/GlobalModal';
 import './boardsPage.css';
 
 export default function BoardsPage() {
@@ -14,14 +12,6 @@ export default function BoardsPage() {
   const token = useAppSelector((state) => state.userSlice.token);
   const userBoards = useAppSelector((state) => state.boardsSlice.userBoards);
   const isLoading = useAppSelector((state) => state.boardsSlice.isLoading);
-  const isModalOpen = useAppSelector((state) => state.modalSlice.isModalOpen);
-  const isRemoveBoard = useAppSelector((state) => state.modalSlice.isRemoveBoard);
-
-  const currentPropComponent = isRemoveBoard ? (
-    localeEN.modalContetntMessage.REMOVE_BOARD_CONFIRM_MESSAGE
-  ) : (
-    <CreateBoardForm />
-  );
   useEffect(() => {
     dispatch(fetchGetUserBoards(token));
   }, [dispatch, token]);
@@ -29,7 +19,6 @@ export default function BoardsPage() {
   return (
     <section className="boards-page">
       <div className="boards-page__container">
-        {isModalOpen && <GlobalModal component={currentPropComponent} />}
         <h1 className="boards-page__title">Boards Page</h1>
         <AddBoardButton />
         <section className="boards-page__container_boards-field">
