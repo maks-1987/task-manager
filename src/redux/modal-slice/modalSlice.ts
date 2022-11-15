49a5ed5f645139f7e21cde5a7f1fe94e3f723658
matvey84 <matvey8407@mail.ru>
2022-11-15 20:55:21 +0300

    feat: add new actions

--- src/redux/modal-slice/modalSlice.ts
+++ src/redux/modal-slice/modalSlice.ts
@@ -3,11 +3,15 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 type InitState = {
   isModalOpen: boolean;
   isRemoveBoard: boolean;
+  isCreateColumnOrTask: boolean;
+  isCreateBoard: boolean;
 };
 
 const initState: InitState = {
   isModalOpen: false,
   isRemoveBoard: false,
+  isCreateColumnOrTask: false,
+  isCreateBoard: false,
 };
 
 export const modalSlice = createSlice({
@@ -17,11 +21,18 @@ export const modalSlice = createSlice({
     setModalOpen(state, action: PayloadAction<boolean>) {
       state.isModalOpen = action.payload;
     },
+    setIsCreateBoard(state, action: PayloadAction<boolean>) {
+      state.isCreateBoard = action.payload;
+    },
     setIsRemoveBoard(state, action: PayloadAction<boolean>) {
       state.isRemoveBoard = action.payload;
+    },
+    setIsCreateColumnOrTask(state, action: PayloadAction<boolean>) {
+      state.isCreateColumnOrTask = action.payload;
     },
   },
 });
 
 export default modalSlice.reducer;
-export const { setModalOpen, setIsRemoveBoard } = modalSlice.actions;
+export const { setModalOpen, setIsRemoveBoard, setIsCreateColumnOrTask, setIsCreateBoard } =
+  modalSlice.actions;
