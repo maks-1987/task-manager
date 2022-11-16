095ca07d7fa0cbc85757f95389d79301bd9af28d
matvey84 <matvey8407@mail.ru>
2022-11-16 14:09:33 +0300

    refactor: moved buttons into columns-button folder

--- src/UI/buttons/ButtonDeleteColumn.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { deleteColumnSVG } from './svgButtons';
-
-export const ButtonDeleteColumn = () => {
-  return <button className="button-delete-column">{deleteColumnSVG()}</button>;
-};
