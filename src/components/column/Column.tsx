424112bef28ed5be03a3abb402ba6ef4a7672e32
matvey84 <matvey8407@mail.ru>
2022-11-16 14:15:37 +0300

    feat: add props for show task content and pass it to deleete button

--- src/components/column/Column.tsx
+++ src/components/column/Column.tsx
@@ -1,12 +1,17 @@
 import React, { useState } from 'react';
 import { Task } from '../task/Task';
 import './column.css';
-import { ButtonDeleteColumn } from '../../UI/buttons/ButtonDeleteColumn';
-import { ButtonNewTask } from '../../UI/buttons/ButtonNewTask';
-import { ButtonNewColumn } from '../../UI/buttons/ButtonNewColumn';
-
-export const Column = () => {
-  const [columnTitle, setColumnTitle] = useState('Default name');
+import { ButtonDeleteColumn } from '../../UI/column-buttons/ButtonDeleteColumn';
+import { ButtonNewTask } from '../../UI/column-buttons/ButtonNewTask';
+import { IComleteBoard } from '../../types/types';
+import { localeEN } from '../../locales/localeEN';
+interface IProp {
+  column: IComleteBoard;
+}
+export const Column = (props: IProp) => {
+  console.log(props);
+  const { id, title, order, tasks } = props.column;
+  const [columnTitle, setColumnTitle] = useState(title);
 
   const handleTitle = (event: React.FormEvent<HTMLInputElement>) => {
     setColumnTitle(event.currentTarget.value);
@@ -14,7 +19,7 @@ export const Column = () => {
 
   return (
     <>
-      <div className="column-item">
+      <div className="column-item" id={id}>
         <div className="column-item__control">
           <input
             className="column-item__title"
@@ -23,18 +28,23 @@ export const Column = () => {
             onChange={handleTitle}
           />
           <ButtonNewTask />
-          <ButtonDeleteColumn />
+          <ButtonDeleteColumn id={id} />
         </div>
-        <Task />
-        <Task />
-        <Task />
-        <Task />
+        <section className="task-list">
+          {tasks?.length === 0 ? (
+            <span className="column-item__message">
+              {localeEN.columnContet.HAVE_NOT_TASK_MESSAGE}
+            </span>
+          ) : (
+            tasks?.map((task) => <Task key={task.id} />)
+          )}
+        </section>
+
         <p className="column-item__add-task">
           <ButtonNewTask />
           Add new task
         </p>
       </div>
-      <ButtonNewColumn />
     </>
   );
 };
