e4836b6bf259be878c05d9d847ab2853ddf027dd
matvey84 <matvey8407@mail.ru>
2022-11-17 12:04:12 +0300

    feat: add handler with onBlur event for change column title, change type into IProp interface

--- src/components/column/Column.tsx
+++ src/components/column/Column.tsx
@@ -3,29 +3,44 @@ import { Task } from '../task/Task';
 import './column.css';
 import { ButtonDeleteColumn } from '../../UI/column-buttons/ButtonDeleteColumn';
 import { ButtonNewTask } from '../../UI/column-buttons/ButtonNewTask';
-import { IComleteBoard } from '../../types/types';
+import { IComleteColumn, IFetchQuery } from '../../types/types';
 import { localeEN } from '../../locales/localeEN';
+import { useAppDispatch, useAppSelector } from '../../redux/hooks';
+import { fetchChangeUserColumn } from '../../redux/columns-slice/columnsFetchRequest';
 interface IProp {
-  column: IComleteBoard;
+  column: IComleteColumn;
 }
 export const Column = (props: IProp) => {
   console.log(props);
   const { id, title, order, tasks } = props.column;
   const [columnTitle, setColumnTitle] = useState(title);
-
+  const dispatch = useAppDispatch();
+  const currentBoardId = useAppSelector((state) => state.boardsSlice.currentBoardId);
+  const token = useAppSelector((state) => state.userSlice.token);
   const handleTitle = (event: React.FormEvent<HTMLInputElement>) => {
     setColumnTitle(event.currentTarget.value);
   };
 
+  const changeColumnTitleHandler = (e: React.FocusEvent<HTMLInputElement>) => {
+    const dataForFetch: IFetchQuery = {
+      columnData: { id, title: e.currentTarget.value, order },
+      boardId: currentBoardId,
+      columnId: id,
+      token,
+    };
+    dispatch(fetchChangeUserColumn(dataForFetch));
+  };
+
   return (
     <>
-      <div className="column-item" id={id}>
+      <div className="column-item" id={id} style={{ order: `${order}` }}>
         <div className="column-item__control">
           <input
             className="column-item__title"
             type="text"
             value={columnTitle}
             onChange={handleTitle}
+            onBlur={(e: React.FocusEvent<HTMLInputElement>) => changeColumnTitleHandler(e)}
           />
           <ButtonNewTask />
           <ButtonDeleteColumn id={id} />
