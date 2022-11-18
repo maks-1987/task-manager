e5f6b2fc7e3b4f59f62f517dab5a81a41d75eb2d
matvey84 <matvey8407@mail.ru>
2022-11-18 13:31:20 +0300

    feat: addTasksButtonHandler for calling modal window

--- src/UI/column-buttons/ButtonNewTask.tsx
+++ src/UI/column-buttons/ButtonNewTask.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { useAppDispatch } from '../../redux/hooks';
+import { setModalOpen, setIsCreateTask } from '../../redux/modal-slice/modalSlice';
 import './buttons.css';
 import { addTaskSVG } from './svgButtons';
 
 export const ButtonNewTask = () => {
+  const dispatch = useAppDispatch();
+  const addTasksButtonHandler = () => {
+    dispatch(setModalOpen(true));
+    dispatch(setIsCreateTask(true));
+  };
   return (
-    <button className="button-new-task" title="add new task">
+    <button onClick={addTasksButtonHandler} className="button-new-task" title="add new task">
       {addTaskSVG()}
     </button>
   );
