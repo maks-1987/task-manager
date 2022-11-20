5bebe775bf201e1698833abd27672e9e19a35fce
matvey84 <matvey8407@mail.ru>
2022-11-20 15:59:08 +0300

    feat: add action for save current column id

--- src/UI/column-buttons/ButtonNewTask.tsx
+++ src/UI/column-buttons/ButtonNewTask.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import { setCurrentColumnId } from '../../redux/columns-slice/columnsSlice';
 import { useAppDispatch } from '../../redux/hooks';
 import { setModalOpen, setIsCreateTask } from '../../redux/modal-slice/modalSlice';
 import './buttons.css';
 import { addTaskSVG } from './svgButtons';
-
-export const ButtonNewTask = () => {
+interface IProp {
+  id: string;
+}
+export const ButtonNewTask = (props: IProp) => {
+  const { id } = props;
   const dispatch = useAppDispatch();
   const addTasksButtonHandler = () => {
+    dispatch(setCurrentColumnId(id));
     dispatch(setModalOpen(true));
     dispatch(setIsCreateTask(true));
   };
