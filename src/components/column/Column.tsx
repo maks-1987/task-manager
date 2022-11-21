044883e88ce23fe612815f239601ca2542cd3068
matvey84 <matvey8407@mail.ru>
2022-11-21 23:55:05 +0300

    refactor: remove useState, handleTitle, replace fetchGetAllUserTasks with fetchGetUserColumnByID actions , repalce value attribute with  defaultValue, add Loader component, pass props to Task component

--- src/components/column/Column.tsx
+++ src/components/column/Column.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from 'react';
+import React, { useMemo } from 'react';
 import { Task } from '../task/Task';
 import './column.css';
 import { ButtonDeleteColumn } from '../../UI/column-buttons/ButtonDeleteColumn';
@@ -6,21 +6,20 @@ import { ButtonNewTask } from '../../UI/column-buttons/ButtonNewTask';
 import { IComleteColumn, IFetchQuery } from '../../types/types';
 import { localeEN } from '../../locales/localeEN';
 import { useAppDispatch, useAppSelector } from '../../redux/hooks';
-import { fetchChangeUserColumn } from '../../redux/columns-slice/columnsFetchRequest';
-import { fetchGetAllUserTasks } from '../../redux/columns-slice/tasksFetchRequest';
+import {
+  fetchChangeUserColumn,
+  fetchGetUserColumnByID,
+} from '../../redux/columns-slice/columnsFetchRequest';
+import Loader from '../loader/Loader';
 interface IProp {
   column: IComleteColumn;
 }
 export const Column = (props: IProp) => {
   const { id, title, order, tasks } = props.column;
-  const [columnTitle, setColumnTitle] = useState(title);
   const dispatch = useAppDispatch();
   const currentBoardId = useAppSelector((state) => state.boardsSlice.currentBoardId);
+  const isLoading = useAppSelector((state) => state.columnsSlice.isLoading);
   const token = useAppSelector((state) => state.userSlice.token);
-
-  const handleTitle = (event: React.FormEvent<HTMLInputElement>) => {
-    setColumnTitle(event.currentTarget.value);
-  };
 
   const changeColumnTitleHandler = (e: React.KeyboardEvent<HTMLInputElement>) => {
     const dataForFetch: IFetchQuery = {
@@ -39,8 +38,9 @@ export const Column = (props: IProp) => {
       columnId: id,
       token,
     };
-    dispatch(fetchGetAllUserTasks(dataForFetch));
-  }, [currentBoardId, dispatch, id, token]);
+    dispatch(fetchGetUserColumnByID(dataForFetch));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [dispatch, id]);
   return (
     <>
       <div className="column-item" id={id} style={{ order: `${order}` }}>
@@ -48,21 +48,21 @@ export const Column = (props: IProp) => {
           <input
             className="column-item__title"
             type="text"
-            value={columnTitle}
+            defaultValue={title}
             placeholder={`${localeEN.tooTipContent.CANNOT_BE_EMPTY_PLACEHOLDER_MESSAGE}`}
-            onChange={handleTitle}
             onKeyUp={(e: React.KeyboardEvent<HTMLInputElement>) => changeColumnTitleHandler(e)}
           />
           <ButtonNewTask id={id} />
           <ButtonDeleteColumn id={id} />
         </div>
         <section className="task-list">
+          {isLoading && <Loader />}
           {tasks?.length === 0 ? (
             <span className="column-item__message">
               {localeEN.columnContet.HAVE_NOT_TASK_MESSAGE}
             </span>
           ) : (
-            tasks?.map((task) => <Task key={task.id} />)
+            tasks?.map((task) => <Task key={task.id} task={task} />)
           )}
         </section>
 
