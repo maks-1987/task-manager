250e0e8d1bf5a05a415ac0d60bd1d8e16c1d673b
matvey84 <matvey8407@mail.ru>
2022-11-22 10:47:03 +0300

    feat: add new state and new action for remove column and task

--- src/redux/modal-slice/modalSlice.ts
+++ src/redux/modal-slice/modalSlice.ts
@@ -3,6 +3,8 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 type InitState = {
   isModalOpen: boolean;
   isRemoveBoard: boolean;
+  isRemoveColumn: boolean;
+  isRemoveTask: boolean;
   isCreateColumn: boolean;
   isCreateTask: boolean;
   isCreateBoard: boolean;
@@ -11,6 +13,8 @@ type InitState = {
 const initState: InitState = {
   isModalOpen: false,
   isRemoveBoard: false,
+  isRemoveColumn: false,
+  isRemoveTask: false,
   isCreateColumn: false,
   isCreateTask: false,
   isCreateBoard: false,
@@ -32,8 +36,14 @@ export const modalSlice = createSlice({
     setIsCreateColumn(state, action: PayloadAction<boolean>) {
       state.isCreateColumn = action.payload;
     },
+    setIsRemoveColumn(state, action: PayloadAction<boolean>) {
+      state.isRemoveColumn = action.payload;
+    },
     setIsCreateTask(state, action: PayloadAction<boolean>) {
       state.isCreateTask = action.payload;
+    },
+    setIsRemoveTask(state, action: PayloadAction<boolean>) {
+      state.isRemoveTask = action.payload;
     },
   },
 });
@@ -42,6 +52,8 @@ export default modalSlice.reducer;
 export const {
   setModalOpen,
   setIsRemoveBoard,
+  setIsRemoveColumn,
+  setIsRemoveTask,
   setIsCreateTask,
   setIsCreateColumn,
   setIsCreateBoard,
