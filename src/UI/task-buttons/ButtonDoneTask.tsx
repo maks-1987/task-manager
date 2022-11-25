e438b950ba593acd20ece6f4e4086924f92b93ea
matvey84 <matvey8407@mail.ru>
2022-11-26 00:20:16 +0300

    feat: add handler for remove task from cuurent colunm and moved it  to done column, change props type, add condition for disabled button if it in the done column

--- src/UI/task-buttons/ButtonDoneTask.tsx
+++ src/UI/task-buttons/ButtonDoneTask.tsx
@@ -1,31 +1,64 @@
 import React from 'react';
-import { setCurrentColumnId, setEditedTaskId } from '../../redux/columns-slice/columnsSlice';
-import { useAppDispatch } from '../../redux/hooks';
-import { setIsEditTask, setModalOpen } from '../../redux/modal-slice/modalSlice';
-import { doneTaskSVG, editTaskSVG } from './svgButtons';
+import { setCurrentColumnId } from '../../redux/columns-slice/columnsSlice';
+import {
+  fetchAddNewUserTasks,
+  fetchRemoveUserTask,
+} from '../../redux/columns-slice/tasksFetchRequest';
+import { useAppDispatch, useAppSelector } from '../../redux/hooks';
+import { IComleteColumn, IFetchQuery, ITask } from '../../types/types';
+import { doneTaskSVG } from './svgButtons';
 import './task-buttons.css';
 
 interface IProp {
   id: string;
-  columnId: string;
+  column: IComleteColumn;
+  task: ITask;
 }
 export const ButtonDoneTask = (props: IProp) => {
-  const { id, columnId } = props;
-
+  const { id } = props;
+  const { title, description, userId } = props.task;
+  const token = useAppSelector((state) => state.userSlice.token);
+  const currentBoardId = useAppSelector((state) => state.boardsSlice.currentBoardId);
+  const userCurrentBoardColumns = useAppSelector(
+    (state) => state.columnsSlice.userCurrentBoard.columns
+  );
   const dispatch = useAppDispatch();
 
-  // const goToModalWindow = (e: React.MouseEvent<HTMLButtonElement>) => {
-  //   dispatch(setIsEditTask(true));
-  //   dispatch(setModalOpen(true));
-  //   dispatch(setEditedTaskId(e.currentTarget.id));
-  //   dispatch(setCurrentColumnId(columnId));
-  // };
+  const doneTaskHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const dataForFetch: IFetchQuery = {
+      boardId: currentBoardId,
+      columnId: props.column.id,
+      taskId: e.currentTarget.id,
+      token,
+    };
+
+    dispatch(setCurrentColumnId(props.column.id));
+    dispatch(fetchRemoveUserTask(dataForFetch));
+    movedDoneTaskToDoneColumn();
+  };
+
+  const movedDoneTaskToDoneColumn = () => {
+    const doneColumnId = userCurrentBoardColumns
+      .filter((column) => column.title === 'done')
+      .at(-1)?.id;
+
+    const dataForFetch: IFetchQuery = {
+      taskData: { title, description, userId },
+      boardId: currentBoardId,
+      columnId: doneColumnId,
+      token,
+    };
+
+    dispatch(fetchAddNewUserTasks(dataForFetch));
+  };
+
   return (
     <>
       <button
+        disabled={props.column.title === 'done'}
         id={id}
         className="button-done-task"
-        // onClick={(e: React.MouseEvent<HTMLButtonElement>) => goToModalWindow(e)}
+        onClick={(e: React.MouseEvent<HTMLButtonElement>) => doneTaskHandler(e)}
       >
         {doneTaskSVG()}
       </button>
