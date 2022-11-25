3178323c915eb443a83b8e3c1ccf55ff3f38edda
matvey84 <matvey8407@mail.ru>
2022-11-26 00:55:43 +0300

    feat: add condition for render h3 tag if column title = 'done' or input if column have else title, condition for column message if colum empty

--- src/components/column/Column.tsx
+++ src/components/column/Column.tsx
@@ -58,13 +58,20 @@ export const Column = (props: IProp) => {
           >
             <hr {...provided.dragHandleProps} />
             <div className="column-item__control">
-              <input
-                className="column-item__title"
-                type="text"
-                defaultValue={title}
-                placeholder={`${localeEN.tooTipContent.CANNOT_BE_EMPTY_PLACEHOLDER_MESSAGE}`}
-                onKeyUp={(e: React.KeyboardEvent<HTMLInputElement>) => changeColumnTitleHandler(e)}
-              />
+              {title === 'done' ? (
+                <h3 className="column-item__title">{title}</h3>
+              ) : (
+                <input
+                  className="column-item__title"
+                  type="text"
+                  defaultValue={title}
+                  placeholder={`${localeEN.tooTipContent.CANNOT_BE_EMPTY_PLACEHOLDER_MESSAGE}`}
+                  onKeyUp={(e: React.KeyboardEvent<HTMLInputElement>) =>
+                    changeColumnTitleHandler(e)
+                  }
+                />
+              )}
+
               <ButtonNewTask column={props.column} />
               <ButtonDeleteColumn column={props.column} />
             </div>
@@ -72,7 +79,9 @@ export const Column = (props: IProp) => {
               {isLoading && <Loader />}
               {tasks?.length === 0 ? (
                 <span className="column-item__message">
-                  {localeEN.columnContet.HAVE_NOT_TASK_MESSAGE}
+                  {title === 'done'
+                    ? localeEN.columnContet.HAVE_NOT_TASK_DONE_MESSAGE
+                    : localeEN.columnContet.HAVE_NOT_TASK_MESSAGE}
                 </span>
               ) : (
                 tasks?.map((task) => <Task key={task.id} task={task} column={props.column} />)
