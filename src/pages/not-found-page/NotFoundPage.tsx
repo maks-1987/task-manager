6b9cd44c809f761e828be6f4343aa96be7b1f668
DenisWilk <info.d1@tut.by>
2022-11-30 12:34:42 +0300

    feat: style NotFound Page

--- src/pages/not-found-page/NotFoundPage.tsx
+++ src/pages/not-found-page/NotFoundPage.tsx
@@ -1,5 +1,22 @@
 import React from 'react';
+import { useAppDispatch, useAppSelector } from '../../redux/hooks';
+import { languages } from '../../locales/languages';
+import GoWelcomePageLink from '../../UI/go-welcome-page-link/GoWelcomePageLink';
+import './notFoundPage.css';
 
-export default function NotFoundPage() {
-  return <div>NotFoundPage</div>;
+function NotFoundPage() {
+  const state = useAppSelector((store) => store.settingsSlise);
+  return (
+    <div className={'not-found-page-container ' + state.themeIndex}>
+      <div className={'return-link-container ' + state.themeIndex}>
+        <GoWelcomePageLink />
+      </div>
+      <h3 className={'not-found-page-code ' + state.themeIndex}>404</h3>
+      <h3 className={'not-found-page-title ' + state.themeIndex}>
+        {languages.notFoundPage[state.languageIndex]}
+      </h3>
+    </div>
+  );
 }
+
+export default NotFoundPage;
