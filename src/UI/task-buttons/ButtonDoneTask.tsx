4c804147a44df4173dcf701c61ecd3f89a0538aa
matvey84 <matvey8407@mail.ru>
2022-12-02 14:22:59 +0300

    fix: remove movedDoneTaskToDoneColumn handler, add into doneTaskHandler fetchMarkTasksAsDone request and setTasksAsDone action

--- src/UI/task-buttons/ButtonDoneTask.tsx
+++ src/UI/task-buttons/ButtonDoneTask.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import { localeEN } from '../../locales/localeEN';
-import { setCurrentColumnId } from '../../redux/columns-slice/columnsSlice';
-import {
-  fetchAddNewUserTasks,
-  fetchRemoveUserTask,
-} from '../../redux/columns-slice/tasksFetchRequest';
+import { setCurrentColumnId, setTasksAsDone } from '../../redux/columns-slice/columnsSlice';
+import { fetchMarkTasksAsDone } from '../../redux/columns-slice/tasksFetchRequest';
 import { useAppDispatch, useAppSelector } from '../../redux/hooks';
 import { IComleteColumn, IFetchQuery, ITask } from '../../types/types';
 import { doneTaskSVG } from './svgButtons';
@@ -17,47 +14,46 @@ interface IProp {
 }
 export const ButtonDoneTask = (props: IProp) => {
   const { id } = props;
-  const { title, description, userId } = props.task;
+  const { title, description, userId, order } = props.task;
   const dispatch = useAppDispatch();
   const token = useAppSelector((state) => state.userSlice.token);
   const currentBoardId = useAppSelector((state) => state.boardsSlice.currentBoardId);
   const userCurrentBoardColumns = useAppSelector(
     (state) => state.columnsSlice.userCurrentBoard.columns
   );
-  const languageIndex = useAppSelector((state) => state.settingsSlise.languageIndex);
 
   const doneTaskHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const doneColumnId = userCurrentBoardColumns
+      .filter((column) =>
+        localeEN.columnContet.DEFAULT_DONE_COLUMN.some((title) => title === column.title)
+      )
+      .at(-1);
+
     const dataForFetch: IFetchQuery = {
+      taskData: {
+        title,
+        description,
+        userId,
+        order,
+        columnId: doneColumnId!.id,
+      },
       boardId: currentBoardId,
       columnId: props.column.id,
       taskId: e.currentTarget.id,
       token,
     };
 
+    dispatch(fetchMarkTasksAsDone(dataForFetch));
+    dispatch(setTasksAsDone(dataForFetch));
     dispatch(setCurrentColumnId(props.column.id));
-    dispatch(fetchRemoveUserTask(dataForFetch));
-    movedDoneTaskToDoneColumn();
-  };
-
-  const movedDoneTaskToDoneColumn = () => {
-    const doneColumnId = userCurrentBoardColumns
-      .filter((column) => column.title === localeEN.columnContet.DEFAULT_DONE_COLUMN[languageIndex])
-      .at(-1)?.id;
-
-    const dataForFetch: IFetchQuery = {
-      taskData: { title, description, userId },
-      boardId: currentBoardId,
-      columnId: doneColumnId,
-      token,
-    };
-
-    dispatch(fetchAddNewUserTasks(dataForFetch));
   };
 
   return (
     <>
       <button
-        disabled={props.column.title === localeEN.columnContet.DEFAULT_DONE_COLUMN[languageIndex]}
+        disabled={localeEN.columnContet.DEFAULT_DONE_COLUMN.some(
+          (lang) => lang === props.column.title
+        )}
         id={id}
         className="button-done-task"
         onClick={(e: React.MouseEvent<HTMLButtonElement>) => doneTaskHandler(e)}
